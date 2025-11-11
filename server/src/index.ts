fc516f2ae083e8dcbcc3e71e75f328e1d4b430ad
dev2kod <devesh4coding@gmail.com>
2025-11-12 00:23:17 +0530

    setup nikita dev

--- server/src/index.ts
+++ server/src/index.ts
@@ -3,11 +3,9 @@ import cors from "cors"
 import { Mongoose } from "mongoose"
 import zod from "zod"
 import dotenv from "dotenv"
-import connectdb from "./configs/db.js"
 
 const app = express()
 
-connectdb();
 app.use(cors())
 app.use(express.json())
 
@@ -18,4 +16,4 @@ app.get("/",(req,res)=>{
 })
 
 
-app.listen(3000,()=>{console.log("running on port 3000")})
+app.listen(4004,()=>{console.log("running on port 4004")})
