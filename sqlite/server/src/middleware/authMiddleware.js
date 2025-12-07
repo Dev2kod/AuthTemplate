b34c05b98d97475d48b8b3b88ceebd121369a1dc
dev2kod <devesh4coding@gmail.com>
2025-12-07 23:54:51 +0530

    started with sqlite

---
+++ sqlite/server/src/middleware/authMiddleware.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const authMiddleware = (req,res,next) => {
+  return (
+    <div>authMiddleware</div>
+  )
+}
+
+export default authMiddleware
---
+++ sqlite/server/src/routes/todoRoutes.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const todoRoutes = () => {
+  return (
+    <div>todoRoutes</div>
+  )
+}
+
+export default todoRoutes
